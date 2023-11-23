8dfedbeb5a13571cf625aa4ca27ab342cca98904
satake0916 <stk_halfmoon@icloud.com>
2023-11-23 15:45:10 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/xt3/Xt3Application.kt
@@ -0,0 +1,11 @@
+package com.example.xt3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Xt3Application
+
+fun main(args: Array<String>) {
+	runApplication<Xt3Application>(*args)
+}
---
+++ src/test/kotlin/com/example/xt3/Xt3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.xt3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Xt3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
