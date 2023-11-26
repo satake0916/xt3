2447655462c7ae5f226de3602061add5b81955c9
satake0916 <stk_halfmoon@icloud.com>
2023-11-26 17:52:35 +0900

    ウィジェット部分を作成した

---
+++ front/src/components/widget/Widgets.tsx
@@ -0,0 +1,29 @@
+import { Search } from '@mui/icons-material'
+import { TwitterTimelineEmbed, TwitterShareButton } from 'react-twitter-embed';
+import React from 'react'
+import "./Widgets.css"
+
+type Props = {}
+
+function Widgets({}: Props) {
+  return (
+    <div className="widgets">
+      <div className="widgets-input">
+        <Search className="widgets-search-icon"></Search>
+        <input
+          placeholder="Search"
+          type="text"></input>
+      </div>
+      <div className="widgets-widgets-container">
+        <h2>What's happening</h2>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="Nintendo"
+          options={{height: 600}}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Widgets
