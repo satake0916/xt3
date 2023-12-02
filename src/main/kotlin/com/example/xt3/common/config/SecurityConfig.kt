bda40671ecbcc7b32ea48fe0ef4316451670d1aa
satake0916 <stk_halfmoon@icloud.com>
2023-12-02 22:15:04 +0900

    ログイン機能を追加した。ただしCSRFは無効にしたままなので注意。

--- src/main/kotlin/com/example/xt3/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/xt3/common/config/SecurityConfig.kt
@@ -1,5 +1,8 @@
 package com.example.xt3.common.config
 
+import com.example.xt3.common.security.SimpleAccessDeniedHandler
+import com.example.xt3.common.security.SimpleAuthenticationFailureHandler
+import com.example.xt3.common.security.SimpleAuthenticationSuccessHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -7,28 +10,37 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+
     @Bean
-    @Throws(Exception::class)
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests { authz ->
-            authz
-                .anyRequest()
-                .permitAll()
+        http.authorizeHttpRequests { requests ->
+            requests
+                .anyRequest().authenticated()
+        }.csrf { csrf ->
+            csrf.disable()
+        }.formLogin { login ->
+            login
+                .loginProcessingUrl("/login").permitAll()
+                .usernameParameter("email")
+                .passwordParameter("pass")
+                .successHandler(SimpleAuthenticationSuccessHandler())
+                .failureHandler(SimpleAuthenticationFailureHandler())
+        }.logout { logout ->
+            logout
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(HttpStatusReturningLogoutSuccessHandler())
+        }.exceptionHandling { ex ->
+            ex
+                .accessDeniedHandler(SimpleAccessDeniedHandler())
+                .accessDeniedHandler(SimpleAccessDeniedHandler())
         }
-            .csrf().disable()
-        /* TODO ログイン処理をREST APIでも実装する。Spring Securityの勉強から。
-        .formLogin { login -> login
-            .loginProcessingUrl("/login")
-            .loginPage("/login")
-            .usernameParameter("email")
-            .passwordParameter("pass")
-            .failureUrl("/login?error")
-            .permitAll()
-        }*/
 
         return http.build()
     }
---
+++ src/main/kotlin/com/example/xt3/common/security/SimpleAccessDeniedHandler.kt
@@ -0,0 +1,17 @@
+package com.example.xt3.common.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+
+class SimpleAccessDeniedHandler : AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        accessDeniedException: AccessDeniedException?
+    ) {
+        response?.sendError(HttpStatus.FORBIDDEN.value())
+    }
+}
