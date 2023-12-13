fa8a9c417f5c27bc1b0c81c7f84c325efeeba861
satake0916 <stk_halfmoon@icloud.com>
2023-12-13 23:21:34 +0900

    convertAccountDtoToAccountResのユニットテストを追加した

--- src/test/kotlin/com/example/xt3/domain/service/AccountServiceTests.kt
+++ src/test/kotlin/com/example/xt3/domain/service/AccountServiceTests.kt
@@ -4,6 +4,7 @@ import com.example.xt3.domain.model.dto.AccountDto
 import com.example.xt3.domain.model.dto.AccountId
 import com.example.xt3.domain.model.dto.UserId
 import com.example.xt3.domain.repository.AccountRepository
+import com.example.xt3.domain.repository.FollowerRepository
 import com.example.xt3.openapi.generated.model.AccountRes
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -13,11 +14,13 @@ import java.time.LocalDateTime
 
 class AccountServiceTests : FunSpec({
     lateinit var mockAccountRepository: AccountRepository
+    lateinit var mockFollowerRepository: FollowerRepository
     lateinit var accountService: AccountService
 
     beforeTest {
         mockAccountRepository = mockk()
-        accountService = AccountService(mockAccountRepository)
+        mockFollowerRepository = mockk()
+        accountService = AccountService(mockAccountRepository, mockFollowerRepository)
     }
 
     context("findByAccountId") {
@@ -54,6 +57,35 @@ class AccountServiceTests : FunSpec({
             } returns null
 
             accountService.findByAccountId("42") shouldBe null
+        }
+    }
+
+    context("convertAccountDtoToAccountRes") {
+        test("normal") {
+            val accountDto = AccountDto(
+                accountId = AccountId("12"),
+                userId = UserId("1"),
+                accountName = "jack",
+                displayName = "Jack",
+                profileDescription = "bitcoin and chill",
+                profileImageUrl = "https://example.com/profile_image.jpg",
+                isPrimary = true,
+                createdAt = LocalDateTime.of(2023, 12, 12, 15, 36),
+                updatedAt = LocalDateTime.of(2023, 12, 12, 23, 15),
+            )
+
+            val actualAccountRes = accountService.convertAccountDtoToAccountRes(accountDto)
+
+            val expectedAccountRes = AccountRes(
+                accountId = "12",
+                displayName = "Jack",
+                accountName = "jack",
+                profileDescription = "bitcoin and chill",
+                profileImageUrl = "https://example.com/profile_image.jpg",
+                isPrimary = true
+            )
+
+            actualAccountRes shouldBe expectedAccountRes
         }
     }
 })
